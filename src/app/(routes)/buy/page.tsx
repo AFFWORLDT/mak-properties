fbeccc1aea389bbaaf0f4bf394815af5fb428ac8
affworldt <affworldtechnologies@gmail.com>
2025-09-29 01:18:59 +0400

    dd

--- src/app/(routes)/buy/page.tsx
+++ src/app/(routes)/buy/page.tsx
@@ -20,7 +20,7 @@ import { BuyCard } from "@/src/view/buy/buyCard";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Loader, Filter, X, Search } from "lucide-react";
 import PropertyCardSkeleton from "@/src/components/common/property-card-skeleton";
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useMemo, Suspense } from "react";
 import { api } from "@/src/lib/axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
@@ -84,7 +84,8 @@ const HANDOVER_YEAR_OPTIONS = [
   "2035",
 ];
 
-function Buy() {
+// Component that uses useSearchParams - needs to be wrapped in Suspense
+function BuyWithSearchParams() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [property, setProperty] = React.useState([]);
@@ -898,6 +899,21 @@ function Buy() {
         </div>
       )}
     </div>
+  );
+}
+
+// Main component that wraps BuyWithSearchParams in Suspense
+function Buy() {
+  return (
+    <Suspense fallback={
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4 container my-4 mx-auto">
+        {Array.from({ length: 6 }).map((_, i) => (
+          <PropertyCardSkeleton key={i} />
+        ))}
+      </div>
+    }>
+      <BuyWithSearchParams />
+    </Suspense>
   );
 }
 
